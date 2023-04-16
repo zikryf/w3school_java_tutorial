8755d4bc08e9e012a8be9e83311343c950d953e1
z1kwy <m07zikry@gmail.com>
2023-04-16 16:54:14 +0800

    Completed Java Syntax

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+// Note: Java is case-sensitive: "MyClass" and "myclass" has different meaning.
+// The name of the java file must match the class name.
+
+// every line of code that runs in Java must be inside a class
+// a class name always start with an uppercase letter
+// the class in this example is: Main
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
+
